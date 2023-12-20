10d5a68409d9c86cc5d3c3e8e5f1c543867e58cc
woooyoung <axdsw121@gmail.com>
2023-12-20 11:41:55 +0900

    1단계, 종료기능 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("123123");
+
+		Scanner sc = new Scanner(System.in);
+
+		new App(sc).run();
+
+		sc.close();
 	}
 }
